b917e6f7e5bd3487c633b58ae19f9a8372361fd9
LiaLobo <lia.lobo@gmail.com>
2019-11-26 15:07:08 -0300

    criada página home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment }from 'react'
-
+import Home from './pages/Home'
 
 // import './App.css'
 
 function App() {
   return (
     <Fragment>
-      
+      <Home />
     </Fragment>
   )
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../../components/Button/Button'
+
+import './styles.css'
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <Button />
+        </div>
+    )
+}
